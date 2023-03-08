39584cd1ab04a3da6fc7104e883fc765f165f7ef
Seok Ho <seokho2463@let-shack.com>
2023-03-08 14:45:13 +0900

    init

--- src/org/example/api/ExampleApplication.java
+++ src/org/example/api/ExampleApplication.java
@@ -24,6 +24,7 @@ public class ExampleApplication {
 
         // 수정 (글 생성, 수정에서 한글로 넣으니까 터미널에서 조회시 ???? 로 나옴 뭔가 이상해서 확인해보니 영어는 잘됨.)
         // 원래는 한글도 잘됐고 나는 아무짓도 안했는데 갑자기 왜이럼?
+
 //        boardController.updateBoard("48b91726-c7e0-4c24-bba4-ff447a780eee");
     }
 }
